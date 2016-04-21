dad161df77e2b6d39bd622e7a3bdb61f6448e3c7
Marcus Hellberg <marcus@vaadin.com>
2016-04-21 12:13:09 -0700

    New demo viewer to support showing other APIs Added angular example to Vaadin Icons

---
+++ src/main/webapp/examples/core/icons/angular2.ts
@@ -0,0 +1,18 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-icons-simple-component',
+    template: `
+    <p>
+      <iron-icon icon="vaadin-icons:arrow-forward"></iron-icon>
+      Vaadin icons can be used anywhere iron-icons can be used in Polymer apps.
+    </p>
+    <p>For instance, in
+      <paper-button raised>
+        <iron-icon icon="vaadin-icons:check"></iron-icon>
+        Buttons
+      </paper-button>
+    </p>
+  `
+})
+export class MyIconsSimpleComponent { }
