a948214116e6792e177628b0f829ef3d477db492
Kari Söderholm <haprog@vaadin.com>
2017-04-19 09:24:38 +0300

    Update bundle.sh so that the sed command works the same on both GNU sed and BSD sed (Linux and macOS)
    There are a lot of differences you must be aware. See:
    - http://stackoverflow.com/questions/5694228/sed-in-place-flag-that-works-both-on-mac-bsd-and-linux
    - http://stackoverflow.com/documentation/sed/9436/bsd-macos-sed-vs-gnu-sed-vs-the-posix-sed-specification

--- bundle.sh
+++ bundle.sh
@@ -3,7 +3,13 @@ WEBAPP_PATH="src/main/webapp"
 
 vulcanize $WEBAPP_PATH/imports.html > $WEBAPP_PATH/all-the-things.html --inline-css --inline-scripts --strip-comments && crisper --source $WEBAPP_PATH/all-the-things.html --html $WEBAPP_PATH/all-the-things_html.html --js $WEBAPP_PATH/all-the-things_js.js && minify $WEBAPP_PATH/all-the-things_js.js
 
-sed -i '' -E "1s/all-the-things_js\.js/all-the-things_js.min.js?$$/g" $WEBAPP_PATH/all-the-things_html.html
-sed -i '' -E '10s/<link.+polymer\/polymer.html">//g' $WEBAPP_PATH/bower_components/hydrolysis/hydrolysis-analyzer.html
+# Note that GNU sed (Linux) and BSD sed (macOS) work very differently
+# see: http://stackoverflow.com/questions/5694228/sed-in-place-flag-that-works-both-on-mac-bsd-and-linux
+# and: http://stackoverflow.com/documentation/sed/9436/bsd-macos-sed-vs-gnu-sed-vs-the-posix-sed-specification
+# This syntax below should work the same on both systems.
+sed -i.bak -E "1s/all-the-things_js\.js/all-the-things_js.min.js?$$/g" $WEBAPP_PATH/all-the-things_html.html
+rm -f $WEBAPP_PATH/all-the-things_html.html.bak
+sed -i.bak -E '10s/<link.+polymer\/polymer.html">//g' $WEBAPP_PATH/bower_components/hydrolysis/hydrolysis-analyzer.html
+rm -f $WEBAPP_PATH/bower_components/hydrolysis/hydrolysis-analyzer.html.bak
 
 $(cd src/main/webapp/examples/pro/board && bower install --quiet)
