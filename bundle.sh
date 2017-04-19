48d6bff4d131ccff6519e1f584b1b375518f8edc
Kari Söderholm <haprog@vaadin.com>
2017-04-19 08:49:56 +0300

    Add Bower dependency installation for vaadin-board demo
    Also fix paths in board.html.

--- bundle.sh
+++ bundle.sh
@@ -5,3 +5,5 @@ vulcanize $WEBAPP_PATH/imports.html > $WEBAPP_PATH/all-the-things.html --inline-
 
 sed -i '' -E "1s/all-the-things_js\.js/all-the-things_js.min.js?$$/g" $WEBAPP_PATH/all-the-things_html.html
 sed -i '' -E '10s/<link.+polymer\/polymer.html">//g' $WEBAPP_PATH/bower_components/hydrolysis/hydrolysis-analyzer.html
+
+$(cd src/main/webapp/examples/pro/board && bower install --quiet)
