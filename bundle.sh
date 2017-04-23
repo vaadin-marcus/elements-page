ea9872044cd9bfa2f986340c4eb29470c9b46eb5
Kari Söderholm <haprog@vaadin.com>
2017-04-23 20:24:49 +0300

    Try to fix Jenkins build failure

--- bundle.sh
+++ bundle.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 WEBAPP_PATH="src/main/webapp"
 
 vulcanize $WEBAPP_PATH/imports.html > $WEBAPP_PATH/all-the-things.html --inline-css --inline-scripts --strip-comments && crisper --source $WEBAPP_PATH/all-the-things.html --html $WEBAPP_PATH/all-the-things_html.html --js $WEBAPP_PATH/all-the-things_js.js && minify $WEBAPP_PATH/all-the-things_js.js
@@ -12,4 +12,8 @@ rm -f $WEBAPP_PATH/all-the-things_html.html.bak
 sed -i.bak -E '10s/<link.+polymer\/polymer.html">//g' $WEBAPP_PATH/bower_components/hydrolysis/hydrolysis-analyzer.html
 rm -f $WEBAPP_PATH/bower_components/hydrolysis/hydrolysis-analyzer.html.bak
 
-$(cd src/main/webapp/examples/pro/board && npm install && bower install --quiet && node_modules/polymer-cli/bin/polymer.js build || true)
+( pushd "$WEBAPP_PATH/examples/pro/board" \
+&& npm install \
+&& bower install --quiet \
+&& node_modules/polymer-cli/bin/polymer.js build \
+; popd )
